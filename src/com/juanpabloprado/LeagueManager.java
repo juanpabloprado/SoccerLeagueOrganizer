e81f7f0bf40da47603970b745e126c68a4d147f9
Juan Pablo Prado <juan@juanpabloprado.com>
2016-05-30 23:40:32 -0500

    Make a chooser menu

--- src/com/juanpabloprado/LeagueManager.java
+++ src/com/juanpabloprado/LeagueManager.java
@@ -9,6 +9,10 @@ public class LeagueManager {
     Player[] players = Players.load();
     System.out.printf("There are currently %d registered players.%n", players.length);
     // Your code here!
+    LeagueManagerMenu menu = new LeagueManagerMenu(
+        new String[] {"Create a new team", "Add players to a team", "Remove players from a team"});
+    Prompter prompter = new Prompter(menu);
+    prompter.displayWelcome();
+    prompter.promptMenu();
   }
-
 }
---
+++ src/com/juanpabloprado/LeagueManagerMenu.java
@@ -0,0 +1,22 @@
+package com.juanpabloprado;
+
+public class LeagueManagerMenu {
+
+  private String[] menuItems;
+
+  public LeagueManagerMenu(String[] menuItems) {
+    this.menuItems = menuItems;
+  }
+
+  public String[] getMenuItems() {
+    return menuItems;
+  }
+
+  public String getFormattedMenu() {
+    String formattedMenu = "";
+    for (int i = 0; i < menuItems.length; i++) {
+      formattedMenu += String.format("[%d] %s%n", i + 1, menuItems[i]);
+    }
+    return formattedMenu;
+  }
+}
