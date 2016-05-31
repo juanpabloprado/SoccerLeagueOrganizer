af7a6fa9f1ed026a4e42562d9d0ce3813abde60c
Juan Pablo Prado <juan@juanpabloprado.com>
2016-05-30 23:39:56 -0500

    Add team model

---
+++ src/com/juanpabloprado/model/Team.java
@@ -0,0 +1,41 @@
+package com.juanpabloprado.model;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class Team {
+  private String name;
+  private String coachName;
+  Set<Player> players;
+
+  public Team(String name, String coachName, Set<Player> players) {
+    this.name = name;
+    this.coachName = coachName;
+    this.players = players;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getCoachName() {
+    return coachName;
+  }
+
+  public Set<Player> getPlayers() {
+    return players;
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Team team = (Team) o;
+    return Objects.equals(name, team.name) &&
+        Objects.equals(coachName, team.coachName) &&
+        Objects.equals(players, team.players);
+  }
+
+  @Override public int hashCode() {
+    return Objects.hash(name, coachName, players);
+  }
+}
