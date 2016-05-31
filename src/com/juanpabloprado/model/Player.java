26621576f3519e40085ca2ef6bb82e6c8281ad67
Juan Pablo Prado <juan@juanpabloprado.com>
2016-05-30 22:56:36 -0500

    Implement compareTo

--- src/com/juanpabloprado/model/Player.java
+++ src/com/juanpabloprado/model/Player.java
@@ -36,7 +36,13 @@ public class Player implements Comparable<Player>, Serializable {
   @Override
   public int compareTo(Player rhs) {
     // We always want to sort by last name then first name
-    return 0;
+    int lastNameCompare = lastName.compareTo(rhs.getLastName());
+    int firstNameCompare = firstName.compareTo(rhs.getFirstName());
+    if (lastNameCompare != 0) {
+      return lastNameCompare;
+    } else {
+      return firstNameCompare;
+    }
   }
 
   @Override
