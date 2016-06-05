ea98327dee8f8002beed017fbb7ef12ed8eb03f7
Juan Pablo Prado <juan@juanpabloprado.com>
2016-06-04 23:22:51 -0500

    Add helper method for printing a list

--- src/com/juanpabloprado/PrompterUtil.java
+++ src/com/juanpabloprado/PrompterUtil.java
@@ -1,5 +1,7 @@
 package com.juanpabloprado;
 
+import java.util.List;
+
 public class PrompterUtil {
   public static void displayWelcome() {
     CharSequence separator = "\n=============\n";
@@ -8,5 +10,12 @@ public class PrompterUtil {
 
   public static void displayError(Exception e, String message) {
     System.out.printf("%s, %s%n", e.getMessage(), message);
+  }
+
+  public static void printPrettyList(List list) {
+    for (int i = 0, listSize = list.size(); i < listSize; i++) {
+      Object item = list.get(i);
+      System.out.printf("%d. %s%n", i + 1, item.toString());
+    }
   }
 }
