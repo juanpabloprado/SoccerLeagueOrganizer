08394bc4a5f8c5ae3352ce590ede05d32c269092
Juan Pablo Prado <juan@juanpabloprado.com>
2016-06-06 20:27:04 -0500

    Remove team listener

--- src/com/juanpabloprado/LeagueManager.java
+++ src/com/juanpabloprado/LeagueManager.java
@@ -4,11 +4,7 @@ import com.juanpabloprado.menu.LeagueManagerMenu;
 import com.juanpabloprado.menu.MenuPrompter;
 import com.juanpabloprado.model.Player;
 import com.juanpabloprado.model.Players;
-import com.juanpabloprado.model.Team;
-import com.juanpabloprado.team.TeamListener;
 import com.juanpabloprado.team.TeamManager;
-import com.juanpabloprado.util.PrompterUtil;
-import java.util.HashSet;
 
 public class LeagueManager {
 
@@ -21,25 +17,6 @@ public class LeagueManager {
         new String[] {"Create a new team", "Add players to a team", "Remove players from a team", "Exit"});
     MenuPrompter menuPrompter = new MenuPrompter(menu);
     final TeamManager teamManager = new TeamManager(players);
-
-    menuPrompter.setTeamListener(new TeamListener() {
-      @Override public void onCreate(String teamName, String teamCoach) {
-        try {
-          teamManager.addTeam(new Team(teamName, teamCoach, new HashSet<Player>()));
-        } catch (TeamManager.TeamException e) {
-          PrompterUtil.displayError(e, "You may want to create more players first.");
-        }
-      }
-
-      @Override public void onPlayerAdded() {
-
-      }
-
-      @Override public void onPlayerRemoved() {
-
-      }
-    });
-
     menuPrompter.setTeamManagerContract(teamManager);
     menuPrompter.start();
   }
--- src/com/juanpabloprado/team/TeamListener.java
+++
@@ -1,9 +0,0 @@
-package com.juanpabloprado.team;
-
-public interface TeamListener {
-  void onCreate(String teamName, String teamCoach);
-
-  void onPlayerAdded();
-
-  void onPlayerRemoved();
-}
